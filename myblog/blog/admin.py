d2c6e93a983e7260ea9e2f3f54ae637fbebf6432
wangli-wangli <33982322+wangli-wangli@users.noreply.github.com>
2018-08-21 19:00:57 +0800

    myblog网站
    myblog网站的改进之后的代码

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from  .models import  Article
-admin.site.register(Article)
+#在admin中显示数据的其他内容
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','content','pub_time')#在admin中显示这三个属性
+    list_filter = ('pub_time',)#admin过滤器，按照pub_time排序
+
+admin.site.register(Article,ArticleAdmin)#在admin中注册Article类,在admin中显示数据的其他内容
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-# 类
-class Article(models.Model):
+# 类3
+class Article(models.Model):#若没有设置主键，数据库中会自动增加id主键属性
     title=models.CharField(max_length=32,default='title')
     content=models.TextField(null=True)
+    pub_time=models.DateTimeField(auto_now=True)#属性pub_time为现在的时间
 
-def __str__(self):
+def __str__(self):#在
          return  self.title
+
+
