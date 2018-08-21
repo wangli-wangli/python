a7e799e0b1e02d2e7ada0d0c4b2cf995b71d6844
wangli-wangli <33982322+wangli-wangli@users.noreply.github.com>
2018-08-21 17:54:41 +0800

    python2的基本语法
    python2的基本语法

---
+++ test_1/1.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+if True:
+#    print "我是中国人！";#注释·
+#    print "TRUE!";
+   raw_input("按下enter键退出，其他键继续显示！....\n");
+else:
+   print"你猜，我是谁！";
+   print "FALSE!";
+
+
+
---
+++ test_1/lalalalala.py
@@ -0,0 +1,12 @@
+'''
+Created on 2018年7月26日
+
+@author: lenovo
+'''
+
+
+
+
+ 
+ 
+ 
